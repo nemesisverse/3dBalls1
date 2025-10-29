3836e8e18c42d65b04cdca182a5ecfc6e0b7bf08
Abhinav Pal <meabhi1222@gmail.com>
2025-10-30 02:23:03 +0530

    moving down

---
+++ T1/Assets/scripts/Tetris block script/TetrisBlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    private float prevTime;
+    public float fallTime = 2f; // Make it public so you can tweak in Inspector
+    public float moveDistance = 0.44f;
+
+    void Update()
+    {
+        if (Time.time - prevTime > fallTime)
+        {
+            transform.position += Vector3.down * moveDistance; // ✅ 'Vector3' must be capitalized
+            prevTime = Time.time;
+        }
+    }
+
+    bool checkValidMove()
+    {
+
+        return true;
+    }
+}
