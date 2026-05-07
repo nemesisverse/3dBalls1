ab8b51c72e55e1004591d5418a18a8d329ddaec4
Abhinav Pal <meabhi1222@gmail.com>
2026-05-07 11:06:50 +0530

    s1 final

--- T1/Assets/scripts/T Blocks script/T2Movement.cs
+++ T1/Assets/scripts/T Blocks script/T2Movement.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Android.Gradle;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class T2Movement : MonoBehaviour, IFallingBlock
