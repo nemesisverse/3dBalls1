65c20449316c8c79f17f63461b42ab93646cefd8
Abhinav Pal <meabhi1222@gmail.com>
2026-06-05 20:39:40 +0530

    soud control working

--- T1/Assets/ButtonManager.cs
+++ T1/Assets/ButtonManager.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject mother;
+    public Slider musicSlider;
 
     void Start()
     {
         pauseMenu.SetActive(false);
+
+        float saved = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        musicSlider.value = saved;
+        AudioListener.volume = saved;   // apply on load
+
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
     }
+
+    public void SetMusicVolume(float volume)
+    {
+        AudioListener.volume = volume;                   // controls ALL audio globally
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
     public void showMenu()
     {
         pauseMenu.SetActive(true);
@@ -20,21 +35,20 @@ public class ButtonManager : MonoBehaviour
     public void Resume()
     {
         pauseMenu.SetActive(false);
-        Time.timeScale = 1f; 
-        mother.SetActive(true); 
-
+        Time.timeScale = 1f;
+        mother.SetActive(true);
     }
 
     public void Retry()
     {
         mother.SetActive(true);
-        Time.timeScale = 1f;  // reset before reload, otherwise scene starts frozen
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void Home()
-{
-    Time.timeScale = 1f;  // reset timeScale in case pause menu is open
-    SceneManager.LoadScene("Main Menu");
-}
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
 }
